befcbc1653ea80c63e84dc6e41954f0033e5e2cc
최홍희 <vvshinevv@gmail.com>
2022-11-12 20:26:16 +0900

    10장. 상속과 구성 따라하기

--- src/main/scala/white/ch10/Chapter10.scala
+++ src/main/scala/white/ch10/Chapter10.scala
@@ -2,7 +2,7 @@ package white.ch10
 
 object Chapter10 extends App {
 
+  val element: Element = new ArrayElement(Array("1", "2", "3")).beside(new ArrayElement(Array("a", "b", "c")))
 
-
-
+  println(element.contents mkString "\n")
 }
