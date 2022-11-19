95093cc922826ef2b3f38e8f686a8c8eb5b45ac0
최홍희 <vvshinevv@gmail.com>
2022-11-19 21:16:01 +0900

    스칼라 따라치기

--- src/main/scala/white/ch09/Chapter09.scala
+++ src/main/scala/white/ch09/Chapter09.scala
@@ -5,43 +5,38 @@ import java.lang.Thread.sleep
 object Chapter09 extends App {
 
   val x = 5
-//  boolAssert(x / 0 == 0)
+//  boolAssert(x / 0 == 0)\
+
   boolMethodAssert(() => {
     println("boolMethodAssert")
     x / 0 == 0
   })
+
+  def boolMethodAssert(predicate: () => Boolean): Unit = {
+    println("aa")
+    println(flag)
+
+    if (flag && !predicate())
+      throw new AssertionError()
+  }
+
   callByNameBoolAssert {
     println("callByNameBoolAssert")
     x / 0 == 0
   }
 
-  var flag = true
+  val flag = true
   def boolAssert(predicate: Boolean): Unit = {
     if (flag && !predicate)
       throw new AssertionError()
   }
 
-  def boolMethodAssert(predicate: () => Boolean): Unit = {
-    if (flag && !predicate())
-      throw new AssertionError()
-  }
+
 
   def callByNameBoolAssert(predicate: => Boolean): Unit = {
     if (flag && !predicate)
       throw new AssertionError()
   }
 
-  def a: Int = {
-    1
-  }
-
-  def bbbb() :Int = {
-    2
-  }
-
-  a
-  bbbb()
-
-  sleep(100)
 }
 
--- src/main/scala/white/ch17/Chapter17.scala
+++ src/main/scala/white/ch17/Chapter17.scala
@@ -6,4 +6,23 @@ object Chapter17 extends App {
   println(result1)
   val result2 = list flatMap { i => i }
   println(result2)
+
+
+  (1 to 5).map(_ * 2)
+
+  def sum(a: Int, b: Int, c: Int): Int = {
+    a + b + c
+  }
+
+  val part = sum(1, _, 2)
+  println(part(5))
+  println("-----------")
+  val result = List(1, 2, 3, 4, 5).map {
+    val x = (_: Int) * 2
+    println("what is x => " + x)
+    x
+  }
+  println(result)
+
+  val empty: List[Int] = Nil
 }
