cc119ac7428d3ba878e1c558eb1247b802fed88f
최홍희 <vvshinevv@gmail.com>
2022-12-10 22:24:52 +0900

    스칼라 책 예제 따라치기

--- src/main/scala/white/ch21/Sample.java
+++ src/main/scala/white/ch21/Sample.java
@@ -4,7 +4,7 @@ import java.util.Random;
 
 public class Sample {
     public static void main(String[] args) {
-        Random random = new Random(10);
+        Random random = new Random();
         for (int i = 0; i < 100; i++) {
             System.out.println(random.nextInt(10));
         }
